efc8d584848228ba6ca3e1586af0db17b93d4f9a
hiro <kiryuinh32@gmail.com>
2022-08-18 22:23:42 +0900

    project init

---
+++ src/test/java/hiro/divelog/DiveLogApplicationTests.java
@@ -0,0 +1,13 @@
+package hiro.divelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiveLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
